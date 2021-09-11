b7060c1685ec3c6fed6acbf8d38e75d36c647b24
Madhusanka331 <cmadhusanka29@gmail.com>
2021-09-11 21:09:06 +0530

    category update
    Category Update

---
+++ app/src/main/java/com/example/All4Pets/itemC.java
@@ -0,0 +1,15 @@
+package com.example.All4Pets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class itemC extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_c);
+    }
+
+}
