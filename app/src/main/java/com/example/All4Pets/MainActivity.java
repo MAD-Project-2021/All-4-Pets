4a537ebfa70250c1fc57e758dc375d2cdb35569e
NilakshiSandeepanie <sandeepanien@gmail.com>
2021-09-13 20:23:31 +0530

    updated files
    updated files

--- app/src/main/java/com/example/All4Pets/MainActivity.java
+++ app/src/main/java/com/example/All4Pets/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.All4Pets;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.FirebaseApp;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
     public void gotofirstpage (View view){
         Intent intent = new Intent(MainActivity.this, Category.class);
---
+++ app/src/main/java/com/example/All4Pets/View_Feedback.java
@@ -0,0 +1,37 @@
+package com.example.All4Pets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class View_Feedback extends AppCompatActivity {
+
+    private ImageView button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_feedback);
+
+        button2 = (ImageView) findViewById(R.id.btn_back);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivityRatings();
+            }
+        });
+
+    }
+
+    public void openActivityRatings(){
+        Intent intent = new Intent(this , Ratings.class);
+        startActivity(intent);
+    }
+
+
+}
