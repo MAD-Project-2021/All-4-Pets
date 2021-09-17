4776ffa884cea2e81500dd3b962854f853a6e9e4
Raveena Bandara <raveenabandara801@gmail.com>
2021-09-18 00:06:03 +0530

    fetch data
    create adapter and other files to fetch data from the database

---
+++ app/src/main/java/com/example/All4Pets/Doctor/Vets.java
@@ -0,0 +1,22 @@
+package com.example.All4Pets.Doctor;
+
+public class Vets {
+
+    String name,speciality,price,rate;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public String getRate() {
+        return rate;
+    }
+}
